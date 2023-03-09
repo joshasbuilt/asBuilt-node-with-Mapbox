e2562c8aef8b57ab29fc91888ba3db1163b4b497
joshnewzealand <joshnewzealand@gmail.com>
2023-03-09 22:18:11 +1300

    const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;

--- server.js
+++ server.js
@@ -1,77 +1,87 @@
 const express = require('express');
-const msal = require('@azure/msal-node');
+const passport = require('passport');
+const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const cookieParser = require('cookie-parser');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-const config = {
-    auth: {
-        clientId: 'f0b08376-6d02-45d9-af3a-3020a70e1c35',
-        authority: 'https://login.microsoftonline.com/f0b08376-6d02-45d9-af3a-3020a70e1c35',
-        clientSecret: 'https://neom-show-legend-app-dev.azurewebsites.net/.auth/login/aad/callback'
+const clientID = 'd623ebfa-820c-49ad-93d6-882c814a087f';
+const clientSecret = '51c0a154-38dc-475e-9f1f-f68724cb296b';
+const tenantID = 'f0b08376-6d02-45d9-af3a-3020a70e1c35';
+const redirectURI = 'https://neom-show-legend-app-dev.azurewebsites.net/.auth/login/aad/callback';
+const scope = 'https://graph.microsoft.com/.default';
+
+// configure passport middleware
+passport.use(new OIDCStrategy({
+    identityMetadata: `https://login.microsoftonline.com/${tenantID}/.well-known/openid-configuration`,
+    clientID: clientID,
+    responseType: 'code id_token',
+    responseMode: 'form_post',
+    redirectUrl: redirectURI,
+    allowHttpForRedirectUrl: true,
+    clientSecret: clientSecret,
+    validateIssuer: true,
+    passReqToCallback: false,
+    scope: scope
+},
+function(iss, sub, profile, accessToken, refreshToken, done) {
+    // this function is called after the user has successfully authenticated with Azure AD
+    // you can add any additional logic here
+    return done(null, profile);
+}));
+
+// configure cookie parser middleware
+app.use(cookieParser());
+
+// define routes
+app.get('/', (req, res) => {
+    // check if the user is authenticated
+    if (req.isAuthenticated()) {
+        // if the user is authenticated, display a welcome message with the user's display name
+        res.send(`Welcome, ${req.user._json.displayName}!`);
+    } else {
+        // if the user is not authenticated, redirect to the login page
+        res.redirect('/login');
     }
+});
+
+app.get('/login', (req, res, next) => {
+    // authenticate the user using the Azure AD strategy
+    passport.authenticate('azuread-openidconnect', { prompt: 'login' })(req, res, next);
+});
+
+app.get('/logout', (req, res) => {
+    // log out the user and redirect to the home page
+    req.logout();
+    res.redirect('/');
+});
+
+app.post('/.auth/login/aad/callback',
+    passport.authenticate('azuread-openidconnect', { failureRedirect: '/', failureFlash: true }),
+    (req, res) => {
+        // redirect to the home page after successful authentication
+        res.redirect('/');
+    }
+);
+
+// define a middleware to check if the user is authenticated
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        // if the user is authenticated, call the next middleware
+        return next();
+    }
+
+    // if the user is not authenticated, redirect to the login page
+    res.redirect('/login');
 };
 
-
-app.get('/images', (req, res) => {
-  fs.readdir(rootDirectoryPath, (error, files) => {
-    if (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
-    
-    const imageFiles = files.filter(file => file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png'));
-    let html = '';
-    imageFiles.forEach(file => {
-      html += `<img src="/${file}" alt="${file}">`;
-    });
-    res.send(html);
-  });
+app.get('/profile', ensureAuthenticated, (req, res) => {
+    // display the user's profile information
+    res.send(`Hello, ${req.user._json.displayName}! Your email address is ${req.user._json.email}.`);
 });
 
-
-
-app.get('/images', (req, res) => {
-fs.readdir(rootDirectoryPath, (error, files) => {
-  if (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-  
-  const imageFiles = files.filter(file => file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png'));
-  let html = '';
-  imageFiles.forEach(file => {
-    html += `<img src="/${file}" alt="${file}">`;
-  });
-  res.send(html);
-});
-});
-
-
-
-
-const cca = new msal.ConfidentialClientApplication(config);
-
-// Middleware function to authenticate the user
-const authMiddleware = async (req, res, next) => {
-    const tokenRequest = {
-        scopes: ['user.read']
-    };
-
-    try {
-        const response = await cca.acquireTokenByClientCredential(tokenRequest);
-        req.accessToken = response.accessToken;
-        next();
-    } catch (error) {
-        res.status(500).send(error);
-    }
-};
-
-// Serve the index page
-app.get('/', authMiddleware, (req, res) => {
-    res.send(`Access token: ${req.accessToken}`);
-});
-
-// Start the server
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+// start the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
 });

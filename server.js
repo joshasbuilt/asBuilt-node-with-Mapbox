6ceb967341f22b655b0f566b85bfccbb39bffeda
joshnewzealand <joshnewzealand@gmail.com>
2023-03-09 22:46:41 +1300

    popup2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const session = require('express-session');
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -12,77 +13,84 @@ const tenantID = 'f0b08376-6d02-45d9-af3a-3020a70e1c35';
 const redirectURI = 'https://neom-show-legend-app-dev.azurewebsites.net/.auth/login/aad/callback';
 const scope = 'https://graph.microsoft.com/.default';
 
+// configure session middleware
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
 // configure passport middleware
 passport.use(new OIDCStrategy({
-    identityMetadata: `https://login.microsoftonline.com/${tenantID}/.well-known/openid-configuration`,
-    clientID: clientID,
-    responseType: 'code id_token',
-    responseMode: 'form_post',
-    redirectUrl: redirectURI,
-    allowHttpForRedirectUrl: true,
-    clientSecret: clientSecret,
-    validateIssuer: true,
-    passReqToCallback: false,
-    scope: scope
+  identityMetadata: `https://login.microsoftonline.com/${tenantID}/.well-known/openid-configuration`,
+  clientID: clientID,
+  responseType: 'code id_token',
+  responseMode: 'form_post',
+  redirectUrl: redirectURI,
+  allowHttpForRedirectUrl: true,
+  clientSecret: clientSecret,
+  validateIssuer: true,
+  passReqToCallback: false,
+  scope: scope
 },
 function(iss, sub, profile, accessToken, refreshToken, done) {
-    // this function is called after the user has successfully authenticated with Azure AD
-    // you can add any additional logic here
-    return done(null, profile);
+  // this function is called after the user has successfully authenticated with Azure AD
+  // you can add any additional logic here
+  return done(null, profile);
 }));
+
+// initialize Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // configure cookie parser middleware
 app.use(cookieParser());
 
 // define routes
 app.get('/', (req, res) => {
-    // check if the user is authenticated
-    if (req.isAuthenticated()) {
-        // if the user is authenticated, display a welcome message with the user's display name
-        res.send(`Welcome, ${req.user._json.displayName}!`);
-    } else {
-        // if the user is not authenticated, redirect to the login page
-        res.redirect('/login');
-    }
+  // check if the user is authenticated
+  if (req.isAuthenticated()) {
+    // if the user is authenticated, display a welcome message with the user's display name
+    res.send(`Welcome, ${req.user._json.displayName}!`);
+  } else {
+    // if the user is not authenticated, redirect to the login page
+    res.redirect('/login');
+  }
 });
 
-app.get('/login', (req, res, next) => {
-  // authenticate the user using the Azure AD strategy in a popup window
-  passport.authenticate('azuread-openidconnect', { prompt: 'login', display: 'popup' })(req, res, next);
-});
-
+app.get('/login', passport.authenticate('azuread-openidconnect', { prompt: 'login', display: 'popup' }));
 
 app.get('/logout', (req, res) => {
-    // log out the user and redirect to the home page
-    req.logout();
-    res.redirect('/');
+  // log out the user and redirect to the home page
+  req.logout();
+  res.redirect('/');
 });
 
 app.post('/.auth/login/aad/callback',
-    passport.authenticate('azuread-openidconnect', { failureRedirect: '/', failureFlash: true }),
-    (req, res) => {
-        // redirect to the home page after successful authentication
-        res.redirect('/');
-    }
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/', failureFlash: true }),
+  (req, res) => {
+    // redirect to the home page after successful authentication
+    res.redirect('/');
+  }
 );
 
 // define a middleware to check if the user is authenticated
 const ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        // if the user is authenticated, call the next middleware
-        return next();
-    }
+  if (req.isAuthenticated()) {
+    // if the user is authenticated, call the next middleware
+    return next();
+  }
 
-    // if the user is not authenticated, redirect to the login page
-    res.redirect('/login');
+  // if the user is not authenticated, redirect to the login page
+  res.redirect('/login');
 };
 
 app.get('/profile', ensureAuthenticated, (req, res) => {
-    // display the user's profile information
-    res.send(`Hello, ${req.user._json.displayName}! Your email address is ${req.user._json.email}.`);
+  // display the user's profile information
+  res.send(`Hello, ${req.user._json.displayName}! Your email address is ${req.user._json.email}.`);
 });
 
 // start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+  console.log(`Server running on port${port}.`);
 });

8b04b35578e478fc8955fcbb85b7afdcf33b7556
joshasbuilt <Joshua Lumley@ASBUILT.local>
2023-05-18 09:47:15 +1200

    first commit on new machine

--- server.js
+++ server.js
@@ -5,15 +5,7 @@ const path = require('path');
 // Set up the MSAL.js configuration object
 
 
-// Define the desired scope
 
-
-// Define an Express.js middleware function to handle authentication
-
-// Define an Express.js middleware function to serve the index page
-
-
-// Create an instance of the Express.js app
 const app = express();
 
 // Add the middleware functions to the app
@@ -26,24 +18,12 @@ app.get('/', (req, res) => {
  
   res.sendFile(__dirname + '/index2.html');
 
+});
 
+app.get('/legend', (req, res) => {
+ //ff
+  res.sendFile(__dirname + '/index.html');
 
-  // Use the access token to make a request to the Microsoft Graph API
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     Authorization: `Bearer ${req.accessToken}`,
-  //   },
-  //   url: 'https://graph.microsoft.com/v1.0/me',
-  // };
-  // request(options, (error, response, body) => {
-  //   if (error) {
-  //     console.log(error);
-  //     res.sendStatus(500);
-  //     return;
-  //   }
-  //   res.send(body);
-  // });
 });
 
 // Start the server
